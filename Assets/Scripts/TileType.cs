56cbc74df688745c7bbc2dbe58319a4ad171aa3a
Bartłomiej Sieczka <retro44@gmail.com>
2018-04-21 16:26:50 +0200

    Tile definition scriptable object class

---
+++ Assets/Scripts/TileType.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "tileType", menuName = "Match-3 Tile type")]
+public class TileType : ScriptableObject {
+    public Sprite sprite;
+    public Color color;
+}
