8badc7fd5b804d27ac6607bc301fb4d9e442ffb9
Bartłomiej Sieczka <retro44@gmail.com>
2018-04-21 16:29:40 +0200

    Tile base class

---
+++ Assets/Scripts/TileBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBase : MonoBehaviour {
+
+    public TileType tileType;
+
+    [SerializeField]
+    private SpriteRenderer renderer;
+
+    public void OnMouseDrag()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.LogFormat("Picked {0}", gameObject.name);
+        Match3.Grab(this);
+    }
+
+    public void OnMouseUp()
+    {
+        Debug.Log("Released");
+        Match3.Release();
+    }
+	
+
+    public void Setup(TileType def)
+    {
+        tileType = def;
+        renderer.sprite = def.sprite;
+        renderer.color = def.color;
+    }
+}
